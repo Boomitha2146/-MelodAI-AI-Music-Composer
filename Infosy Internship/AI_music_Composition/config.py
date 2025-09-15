9eab7927e9a0cb76794a5a5bd94eb992f75ed3dc
Boomitha B <boomitha1426@gmail.com>
2025-09-15 19:15:51 +0530

    Delete Infosy Internship directory

--- Infosy Internship/AI_music_Composition/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SENTIMENT_MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-    EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-    MAX_LENGTH = 128
-    DEVICE = "cpu"
-    MOOD_CATEGORIES = ["happy", "sad", "calm", "energetic", "mysterious", "romantic"]
